4155d95c12c9c842ef6bff426e37d36d6b45742c
yabushi <shigeki.yabune.0203@gmail.com>
2020-05-18 18:39:14 +0900

    make Location class.

---
+++ app/src/main/java/com/yabunepro/get_Livedoor_weather/Location.java
@@ -0,0 +1,19 @@
+package com.yabunepro.get_Livedoor_weather;
+
+public class Location {
+    private String area;
+    private String pref;
+    private String city;
+
+    public String getArea() {
+        return area;
+    }
+
+    public String getPref() {
+        return pref;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
