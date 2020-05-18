b458d37f8feb95c42953a2ee6f8053106cb11acc
yabushi <shigeki.yabune.0203@gmail.com>
2020-05-18 16:05:34 +0900

    add Implementation that GsonConverterFactory. write retrofits code into MainActivity without Override methods contents.

--- app/src/main/java/com/yabunepro/get_Livedoor_weather/MainActivity.java
+++ app/src/main/java/com/yabunepro/get_Livedoor_weather/MainActivity.java
@@ -3,12 +3,47 @@ package com.yabunepro.get_Livedoor_weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvTitle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvTitle = findViewById(R.id.tvTitle);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://weather.livedoor.com/forecast/webservice/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+
+        Call<List<MinimumWeatherInfo>> call = jsonPlaceHolderApi.getMinimumWeatherInfo();
+
+        call.enqueue(new Callback<List<MinimumWeatherInfo>>() {
+            @Override
+            public void onResponse(Call<List<MinimumWeatherInfo>> call, Response<List<MinimumWeatherInfo>> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<List<MinimumWeatherInfo>> call, Throwable t) {
+
+            }
+        });
     }
 }
