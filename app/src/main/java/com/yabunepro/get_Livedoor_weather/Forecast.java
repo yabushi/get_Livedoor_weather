6aadb4bac7c967f0af4eb29804ab66070a9e85db
yabushi <shigeki.yabune.0203@gmail.com>
2020-05-18 20:39:19 +0900

    make Forcast class.

---
+++ app/src/main/java/com/yabunepro/get_Livedoor_weather/Forecast.java
@@ -0,0 +1,30 @@
+package com.yabunepro.get_Livedoor_weather;
+
+public class Forecast {
+
+    private String date;
+    private String dateLabel;
+    private String telop;
+    private Image image;
+    private Temperature temperature;
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getDateLabel() {
+        return dateLabel;
+    }
+
+    public String getTelop() {
+        return telop;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public Temperature getTemperature() {
+        return temperature;
+    }
+}
