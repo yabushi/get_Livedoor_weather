0833d20991e9b1648c9fb58fe51cde6e1e4a80f8
yabushi <shigeki.yabune.0203@gmail.com>
2020-05-18 14:51:58 +0900

    fix @GET("json") to "@GET("json/v1")

--- app/src/main/java/com/yabunepro/get_Livedoor_weather/JsonPlaceHolderApi.java
+++ app/src/main/java/com/yabunepro/get_Livedoor_weather/JsonPlaceHolderApi.java
@@ -6,7 +6,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface JsonPlaceHolderApi {
-    @GET("json")
+    @GET("json/v1")
     Call<List<MinimumWeatherInfo>> getMinimumWeatherInfo();
 
 }
